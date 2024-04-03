fb54583cdd98b96fa7d00a577043d883ec3713d5
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-03 18:47:08 +0000

    A FIFOCache class

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,12 +13,14 @@ class FIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                discarded_key = next(iter(self.cache_data))
-                del self.cache_data[discarded_key]
-                print("DISCARD:", discarded_key)
+        """Adds an item in the cache."""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            del self.cache_data[key]
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key, _ = self.cache_data.popitem(last=False)
+            print("DISCARD:", discarded_key)
             self.cache_data[key] = item
 
     def get(self, key):
