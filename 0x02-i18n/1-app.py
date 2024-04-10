ed6a8b13cb2d5c1a7152a212160818ed44c99747
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-10 14:41:38 +0000

    A Flask app with babel setting default locale

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,6 +7,7 @@ from flask_babel import Babel
 
 
 class Config:
+    """Represents a Flask Bbael config"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
