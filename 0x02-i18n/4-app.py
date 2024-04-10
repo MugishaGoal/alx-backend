1e2cc47284baa1bf5ce37afe87526e6aacd27e37
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-10 16:30:38 +0000

    A Flask app that force a particular locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""A Flask app that force a particular locale"""
-
-
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Represents a Flask Babel config"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -21,7 +21,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Check if the 'locale' parameter is present in the request URL"""
+    """Retrieves the locale for a web page.
+    """
     queries = request.query_string.decode('utf-8').split('&')
     query_table = dict(map(
         lambda x: (x if '=' in x else '{}='.format(x)).split('='),
@@ -35,9 +36,10 @@ def get_locale() -> str:
 
 @app.route('/')
 def get_index() -> str:
-    """Default page"""
+    """The home/index page.
+    """
     return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
