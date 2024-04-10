b3e906c2711c9d4d23c15d7972ca8fee92e59fab
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-10 16:18:12 +0000

    A Flask app that force a particular locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,34 +2,40 @@
 """A Flask app that force a particular locale"""
 
 
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """Represents a Flask Babel config"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-# Define the supported languages
-SUPPORTED_LANGUAGES = ['en', 'fr']
-
-
-# Set the default locale and timezone
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
-
-
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Check if the 'locale' parameter is present in the request URL"""
-    locale = request.args.get('locale')
-    if locale in SUPPORTED_LANGUAGES:
-        return locale
-    return app.config['BABEL_DEFAULT_LOCALE']
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def index():
+def get_index() -> str:
+    """Default page"""
     return render_template('4-index.html')
 
 
